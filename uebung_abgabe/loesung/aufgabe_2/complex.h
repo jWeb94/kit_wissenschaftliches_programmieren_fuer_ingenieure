1206395fefef72d7711d70481942c4f4c823a039
Jens <weber@fzi.de>
2018-12-14 20:12:18 +0100

    finished programming task 1 and created folder and necessary files (makefile and copyed the in task 1 written complex-class) for task 2

---
+++ uebung_abgabe/loesung/aufgabe_2/complex.h
@@ -0,0 +1,48 @@
+// Compiler-Direktive, um Mehrfacheinbindung zu verhindern
+#ifndef MYCOMPLEXNUM_
+#define MYCOMPLEXNUM_
+
+
+class MyComplex{
+
+public:
+
+  // Konstruktoren
+  MyComplex();                                      // Standartkonstruktor
+  MyComplex(const double xVal, const double yVal);  // Konstruktor mit direkter Angabe a+ib
+  MyComplex(const MyComplex & complexNumber);       // Kopierkonstruktor
+
+  // Destruktoren
+  ~MyComplex(); 									// man koennte auch den Default-Destruktor generisch vom Compiler erstellen lassen, da in dieser Klasse nur Standartdatentypen vorkommen und daher der Speicher nicht selbst verwaltet werden muss!
+
+  // Operatoren
+    // Rechenoperatoren
+  MyComplex operator+(const MyComplex & additionComplex);             	// darf nicht konstant sein, da es in der main_.cpp (z1+z2)+2 aufgerufen wird! Also das enstandene Element noch veraendert wird (+2 genommen) -> const MyComplex operator+(const MyComplex & additionComplex); // geht nicht!
+  const MyComplex operator+(const double additionConstant);
+  const MyComplex operator-(const MyComplex & subtractionComplex);
+  const MyComplex operator*(const MyComplex & multiplicationComplex);
+  MyComplex operator*(const double multiplicationConstant);           	// darf ebenfalls nicht const sein, selber Grund wie +-Operator
+  MyComplex & operator-();                                            	// unaeres Minus. Rueckgabe als Referenz, da das Objekt selbst veraendert wird! veraendert wird!
+    // Zuweisungsoperatoren
+  MyComplex & operator=(const MyComplex & assignmentComplex);  			// Entspricht Kopieroperator
+
+
+  // Methoden
+    // Getter-Methoden
+  const double norm() const;
+  const double real() const;
+  const double imag() const;
+
+  void printComponents(); // debug purpose
+    //Setter-Methoden
+
+private:
+
+  // Membervaribalen
+  double x;
+  double y;
+    // z = x+iy
+
+
+};
+#endif
---
+++ uebung_abgabe/loesung/aufgabe_2/main_konvergenz.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+#include"complex.h" // "", da in aktuellem Working-Directory
+
+using namespace std;
+
+int main (){
+  // TODO: here comes some cool code ;-)
+  cout << "Hello World" << endl;
+
+  return 0;
+}
